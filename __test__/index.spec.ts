8a7d745cb34172ef81b10dcf32d533eb103e711a
NovusTheory <3434404+NovusTheory@users.noreply.github.com>
2026-09-11 14:12:40 -0500

    test: Implement more tests

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -2,7 +2,7 @@
 
 import test from 'ava'
 
-import { MediaPlayer } from '../index.js'
+import { MediaPlayer, MediaPlayerThumbnail } from '../index.js'
 
 function getRandomServiceName() {
   return `test-xosms-${crypto.randomUUID()}`
@@ -27,4 +27,36 @@ test('cannot use MediaPlayer backend after dispose', async (t) => {
   await t.throwsAsync(async () => await mediaPlayer.update());
   await t.throwsAsync(async () => await mediaPlayer.deactivate());
   await t.throwsAsync(async () => await mediaPlayer.setTimeline(0, 0));
+})
+
+test('can set and update all MediaPlayer properties', async (t) => {
+  const mediaPlayer = new MediaPlayer(getRandomServiceName(), "Test Xosms");
+  mediaPlayer.albumTitle = "test";
+  mediaPlayer.artist = ["test", "test"];
+  mediaPlayer.fullscreen = true;
+  mediaPlayer.loop = "track";
+  mediaPlayer.maximumPlaybackRate = 2.0;
+  mediaPlayer.minimumPlaybackRate = 0.25;
+  mediaPlayer.nextButtonEnabled = true;
+  mediaPlayer.pauseButtonEnabled = true;
+  mediaPlayer.playButtonEnabled = true;
+  mediaPlayer.playbackRate = 0.75;
+  mediaPlayer.playbackStatus = "playing";
+  mediaPlayer.previousButtonEnabled = true;
+  mediaPlayer.seekEnabled = true;
+  mediaPlayer.shuffle = true;
+  mediaPlayer.stopButtonEnabled = true;
+  mediaPlayer.thumbnail = await MediaPlayerThumbnail.create("uri", "https://placehold.co/1x1.png");
+  mediaPlayer.title = "test";
+  mediaPlayer.trackId = "test";
+  mediaPlayer.volume = 0.5;
+  await mediaPlayer.activate();
+  await t.notThrowsAsync(async () => await mediaPlayer.update());
+  await t.notThrowsAsync(async () => await mediaPlayer.setTimeline(60, 30));
+  mediaPlayer.dispose();
+})
+
+test('can create MediaPlayerThumbnail of all types', async (t) => {
+  await t.notThrowsAsync(async () => await MediaPlayerThumbnail.create("uri", "https://placehold.co/1x1.png"));
+  await t.notThrowsAsync(async () => await MediaPlayerThumbnail.create("file", "./thumbnail_test.png"));
 })
