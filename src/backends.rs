63dd0a26f004c8f89245165b58bd50a2c3d6cda2
NovusTheory <3434404+NovusTheory@users.noreply.github.com>
2026-09-12 14:40:53 -0500

    fix: macos backend not being included correctly

--- src/backends.rs
+++ src/backends.rs
@@ -6,6 +6,11 @@ pub mod windows;
 #[cfg(target_os = "windows")]
 pub use self::windows as backend;
 
+#[cfg(target_os = "macos")]
+pub mod macos;
+#[cfg(target_os = "macos")]
+pub use self::macos as backend;
+
 #[cfg(target_os = "linux")]
 pub mod linux;
 #[cfg(target_os = "linux")]
